c37a1ee329bddb88806a32b018a3e3da3f9189bd
Dactry <development@sgd.com.au>
2025-03-19 13:58:33 +0200

    Version: 5.9.19

--- single.php
+++ single.php
@@ -5,22 +5,22 @@ while (have_posts()) :
 	$post_type_post = get_post_type() === 'post';
 ?>
 	<div class="row g-0 min-height-full">
-		<div class="col-md-6 pos-rel section-py d-flex has-bg align-items-center">
-			<div class="stretch">
-				<div class="element-sticky">
-					<?php get_template_part('components/background-image', null, ['img_id' => get_post_thumbnail_id(), 'curtain' => true]) ?>
+		<div class="col-md-6 pos-sticky-md align-self-start">
+			<div class="min-height-full section-py d-flex align-items-center">
+				<?php get_template_part('components/background-image', null, ['img_id' => get_post_thumbnail_id(), 'curtain' => true]) ?>
+				<div class="container-sm text-center section-py" data-animate="">
+					<div class="">
+						<?php if ($post_type_post): ?>
+							<a href="<?= esc_url(get_permalink(get_option('page_for_posts'))) ?>" class="hstack justify-content-center align-items-center gap-3"><?= get_core_icon('arrow', 'rotate-180 fs-lg') . get_the_date(); ?></a>
+						<?php endif ?>
+
+						<h1><?= get_the_title() ?></h1>
+
+						<?php if ($post_type_post): ?>
+							<p class="hstack justify-content-center align-items-center gap-2"><?= get_core_icon('calendar') . get_the_date(); ?></p>
+						<?php endif ?>
+					</div>
 				</div>
-			</div>
-			<div class="container-sm text-center" data-animate="">
-				<?php if ($post_type_post): ?>
-					<a href="<?= esc_url(get_permalink(get_option('page_for_posts'))) ?>" class="hstack justify-content-center align-items-center gap-3"><?= get_core_icon('arrow', 'rotate-180 fs-lg') . get_the_date(); ?></a>
-				<?php endif ?>
-
-				<h1><?= get_the_title() ?></h1>
-
-				<?php if ($post_type_post): ?>
-					<p class="hstack justify-content-center align-items-center gap-2"><?= get_core_icon('calendar') . get_the_date(); ?></p>
-				<?php endif ?>
 			</div>
 		</div>
 		<div class="col-md-6 pos-rel section-py d-flex align-items-center">
